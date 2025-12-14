c6874fa6633084d09c64efca51e3f3d0527b650e
Connor Young <connor.young@vitinitiative.com>
2025-12-14 09:01:44 -0500

    Working on API that connects to chatbot

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
+from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 from langchain.tools import tool
 from pydantic import BaseModel
 import uvicorn
 from ddgs import DDGS
-
-import time
 
 app = FastAPI()
 
@@ -26,7 +25,6 @@ app.add_middleware(
 class ChatRequest(BaseModel):
     message: str
 
-@tool
 def search_duckduckgo(query: str) -> str:
     """
     Searches the search engine DuckDuckGo and returns the content from the various web pages
@@ -36,13 +34,23 @@ def search_duckduckgo(query: str) -> str:
     :return: Description
     :rtype: str
     """
-    result = DDGS().text(query, max_results=1)
-    print(result)
-    return "Hey there folks!"
+    results = []
+    with DDGS() as ddgs:
+        for r in ddgs.text(query, max_results=10):
+            results.append(f"{r['title']} - {r['body']}")
+            
+    return "\n".join(results)
 
-llm = ChatOllama(
-    model="gemma3:1b",
-    temperature=0
+@tool
+def web_search(query: str) -> str:
+    """Searches the web for a `query` and returns the title of each result from it."""
+    return search_duckduckgo(query=query)
+
+llm = ChatOllama(model="llama3.2:latest", temperature=0)
+
+agent = create_agent(
+    model=llm,
+    tools=[web_search]
 )
 
 llm_with_tools = llm.bind_tools([search_duckduckgo])
@@ -56,15 +64,22 @@ async def read_root():
     response = llm_with_tools.invoke(messages)
     return {"Hello": response.content}
 
-def stream_answer(message):
-    messages = [
-        ("human", message)
-    ]
-
-    for chunk in llm.stream(messages):
-        event_str = "event: stream_event"
-        yield f"{event_str}\ndata: {chunk.content}\n\n"
-        time.sleep(0.1)
+def stream_answer(message: str):
+    for chunk in agent.stream(
+        {
+            "messages": [
+                {"role": "system", "content": """You are a helpful assistant. If you cannot find current information, use the tool to search the web for the newest information.""" },
+                {"role": "user", "content": message}
+            ]
+        },
+        stream_mode="updates"
+    ):
+        for step, data in chunk.items():
+            print(f"step: {step}")
+            print(f"content: {data['messages'][-1].content_blocks}")
+            event_str = "event: stream_event"
+            content = "stuff"
+            yield f"{event_str}\ndata: {content}\n\n"
 
 @app.post("/chat")
 def generate_chat_response(body: ChatRequest):
